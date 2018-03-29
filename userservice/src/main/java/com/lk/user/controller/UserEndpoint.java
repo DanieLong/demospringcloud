b51f3bdf894bdc9a880293b4e0a3f66a891bd1cd
longke <longke@cictec.cn>
2018-03-29 17:03:17 +0800

    添加一些模块

---
+++ userservice/src/main/java/com/lk/user/controller/UserEndpoint.java
@@ -0,0 +1,32 @@
+package com.lk.user.controller;
+
+
+import com.lk.product.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * User API服务
+ *
+
+ */
+@RestController
+@RequestMapping("/users")
+public class UserEndpoint {
+    protected Logger logger = LoggerFactory.getLogger(UserEndpoint.class);
+
+    @Value("${server.port:2200}")
+    private int serverPort = 2200;
+
+    @RequestMapping(value = "/{loginName}", method = RequestMethod.GET)
+    public User detail(@PathVariable String loginName) {
+        String memos = "I come form " + this.serverPort;
+        return new User(loginName, loginName, "/avatar/default.png", memos);
+    }
+
+
+}
